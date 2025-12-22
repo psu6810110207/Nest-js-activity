5839b9359bd9d7447ed4a5f921c12dfc20e5b57e
Noraphat Kumburee <6810110167@psu.ac.th>
2025-12-22 10:23:19 +0700

    phase 3

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,38 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseGuards,
+  Delete,
+  Param,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
-import { UpdateBookDto } from './dto/update-book.dto';
+
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
+import { UserRole } from '../users/entities/user.entity';
 
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
-  @Post()
-  create(@Body() createBookDto: CreateBookDto) {
-    return this.bookService.create(createBookDto);
-  }
-
+  // Public
   @Get()
   findAll() {
     return this.bookService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.bookService.findOne(id);
+  // Admin Only: CREATE
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @Post()
+  create(@Body() createBookDto: CreateBookDto) {
+    return this.bookService.create(createBookDto);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
-    return this.bookService.update(id, updateBookDto);
-  }
-
+  // ✅ Admin Only: DELETE (จุดที่ขาดไป)
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(UserRole.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.bookService.remove(id);
   }
-  @Patch(':id/like')
-  async likeBook(@Param('id') id: string) {
-    return this.bookService.incrementLikes(id);
-  }
 }
+
+
